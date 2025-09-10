f31b1d8100da297d44b612595e5651cfd42f6b68
AyodejiOmole <omoledaniel13@gmail.com>
2025-09-10 14:43:08 +0100

    tried fixing deployment error.

--- app/api/paystack/initialize/route.ts
+++ app/api/paystack/initialize/route.ts
@@ -1,8 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { rtdb } from '@/lib/firebaseAdmin';
 // import crypto from 'crypto';
-// import { Order, Product } from '@/types';
-import { validateInventory } from '@/lib/utils';
+import { Order } from '@/types';
 
 export const runtime = 'nodejs';
 
@@ -12,6 +11,27 @@ type InitBody = {
   callbackUrl?: string; // optional if you want Paystack to redirect here
 };
 
+export async function validateInventory(order: Order): Promise<{ ok: boolean, failures: string[] }> {
+  // aggregate qty per product+variant (to catch duplicates in same order)
+  const sums = new Map<string, number>();
+  for (const it of order.items) {
+    const key = `${it.id}`;
+    sums.set(key, (sums.get(key) || 0) + it.quantity);
+  }
+
+  const failures: string[] = [];
+  for (const [key, qty] of sums) {
+    // const [productId, variantId] = key.split('::');
+    const snap = await rtdb.ref(`products/${key}/stock`).get();
+    const stock = snap.exists() ? Number(snap.val()) : 0;
+    if (stock < qty) {
+      const productSnap = await rtdb.ref(`products/${key}`).get();
+      const product = productSnap.val();
+      failures.push(`${product.name} only ${stock} left`);
+    }
+  }
+  return { ok: failures.length === 0, failures };
+}
 
 export async function POST(req: NextRequest) {
   try {
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { rtdb } from "./firebaseAdmin";
-import { Order } from "@/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -13,25 +11,3 @@ export const concatenateArray = (arr: string[]): string => {
     .map(str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase())
     .join(', ');
 };
-
-export async function validateInventory(order: Order): Promise<{ ok: boolean, failures: string[] }> {
-  // aggregate qty per product+variant (to catch duplicates in same order)
-  const sums = new Map<string, number>();
-  for (const it of order.items) {
-    const key = `${it.id}`;
-    sums.set(key, (sums.get(key) || 0) + it.quantity);
-  }
-
-  const failures: string[] = [];
-  for (const [key, qty] of sums) {
-    // const [productId, variantId] = key.split('::');
-    const snap = await rtdb.ref(`products/${key}/stock`).get();
-    const stock = snap.exists() ? Number(snap.val()) : 0;
-    if (stock < qty) {
-      const productSnap = await rtdb.ref(`products/${key}`).get();
-      const product = productSnap.val();
-      failures.push(`${product.name} only ${stock} left`);
-    }
-  }
-  return { ok: failures.length === 0, failures };
-}
